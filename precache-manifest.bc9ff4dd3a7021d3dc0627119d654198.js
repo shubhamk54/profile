f239bdab9135d448525e6ce9bc8559eda33ed009
shubhamk54 <shubhamkale54@gmail.com>
2023-01-25 16:27:35 +0000

    Deploying to gh-pages from  @ ad490ff522993802546cffffabc95910193cbac0 🚀

---
+++ precache-manifest.bc9ff4dd3a7021d3dc0627119d654198.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5b18a1b7ff34dad2af45567bdc73a7",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "b5efc42b7784f76cd6b0",
+    "url": "/profile/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "19d5a043fb0dc35fe184",
+    "url": "/profile/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "5d4780b7e169628e2bc2",
+    "url": "/profile/static/css/main.f6e9d026.chunk.css"
+  },
+  {
+    "revision": "e6f5218bc812b803b814",
+    "url": "/profile/static/js/2.880b9f4a.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/profile/static/js/2.880b9f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5efc42b7784f76cd6b0",
+    "url": "/profile/static/js/3.708b81a0.chunk.js"
+  },
+  {
+    "revision": "19d5a043fb0dc35fe184",
+    "url": "/profile/static/js/4.2a29a5f0.chunk.js"
+  },
+  {
+    "revision": "5d4780b7e169628e2bc2",
+    "url": "/profile/static/js/main.a0f4fe2d.chunk.js"
+  },
+  {
+    "revision": "a73bcbc709460a146403",
+    "url": "/profile/static/js/runtime-main.4abd8fdc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/profile/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/profile/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/profile/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/profile/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "a9a5a55569357d77ddb333636ebf8719",
+    "url": "/profile/static/media/excellence_1.a9a5a555.jpeg"
+  },
+  {
+    "revision": "d2c119dada54152fc351a34b28b2325a",
+    "url": "/profile/static/media/excellence_2.d2c119da.jpeg"
+  },
+  {
+    "revision": "0c8a3c3daba3c6eabfe8807a616aea84",
+    "url": "/profile/static/media/kdkLogo.0c8a3c3d.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/profile/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/profile/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/profile/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "5b51ac566407a4f4e75f47bbec00b782",
+    "url": "/profile/static/media/v_award.5b51ac56.jpeg"
+  }
+]);
